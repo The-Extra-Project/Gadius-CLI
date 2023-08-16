eaa7151d5d3b3ed259511603bc56a69253d459ce
Dhruv <malikdhruv1994@gmail.com>
2023-08-16 16:03:40 +0200

    streamlit frontend changes and build settings

--- apps/tile-rendering/app.py
+++ apps/tile-rendering/app.py
@@ -2,25 +2,31 @@ import streamlit as st
 import pandas as pd
 import requests
 import numpy as np
-import pydeck as pdk
-from component.threeDTilerendererJS import threeTileRenderer
+from component.TilerendererJS import threeTileRenderer
 import argparse
-def app(cliargs = None):
-    
-    
-    
+
+
+def app():
     """
     function that runs the  threeDTilerendererJS component. takes the input from cli
-    
     """
     
-    args = argparse.ArgumentParser(description="enter the parameters to render the 3DTiles")
-    args.add_argument("ipfs_cid_pipeline", help="final pipeline.json CID that user wants to render")
-    args.add_argument("filename", help="name of the file to be fetched from given CID")
-    parsedargs = args.parse_args()
+    # args = argparse.ArgumentParser(description="enter the parameters to render the 3DTiles")
+    # args.add_argument("ipfs_cid_pipeline", help="final pipeline.json CID that user wants to render")
+    # args.add_argument("filename", help="name of the file to be fetched from given CID")
+    # parsedargs = args.parse_args()
     st.title("rendered tile")
-    with st.expander("tileset rendered"):
-        threeTileRenderer(parsedargs.ipfs_cid_pipeline,parsedargs.filename)
-    ## TODO: adding other functions in 3DTilerendering in order to render zoomed version of the given rendered file
-    st.button("zoom-in")
-    st.button("zoom out")
+    with st.expander("add details"):
+        # ipfs_cid_pipeline = 'bafybeieoxxdf73ptkw3uqgx225fxpacc6gxdmvb3t5hggtnjmro72e3wl4'
+        # filename = 'pipeline_gen'
+        button = st.button("render tileset")
+        
+        ipfs_cid_pipeline = st.text_input("enter the tileset that you want to render")
+        filename = st.text_input("enter the pipeline file")
+        if button:
+            threeTileRenderer(ipfs_cid_pipeline,filename)
+    # st.button("zoom-in")
+    # st.button("zoom out")
+
+if __name__ == "__main__":
+    app()
